06d781814424046ee228bfb7749c94c64584863d
cemgunal12 <126788995+cemgunal12@users.noreply.github.com>
2023-03-12 14:22:16 +0300

    kullanici giris eklendi

---
+++ kullanicigirisi.java
@@ -0,0 +1,40 @@
+package patikaw2;
+
+import java.util.Scanner;
+
+public class kullanicigirisi {
+    public static void main(String[] args) {
+        String userName,password;
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your user name");
+        userName=input.nextLine();
+
+        System.out.println("Enter your password");
+        password=input.nextLine();
+
+        if(userName.equals("patika")){
+            if(password.equals("java123")){
+                System.out.println("Başarı ile giriş yaptınız");
+            }else{
+                System.out.println("Şifreniz yanlış şifrenizi sıfırlamak istermisiniz\n Evet\nHayır");
+                String cevap=input.nextLine();
+                if(cevap=="Evet"){
+                    System.out.println("Yeni şifrenizi giriniz");
+                    String yeniSifre=input.nextLine();
+                    if(yeniSifre.equals("java123")){
+                        System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
+                    }
+                    else{
+                        System.out.println("Şifre oluşturuldu");
+                    }
+                }else{
+                    System.out.println("O zaman hatırla şifreni kardeş");
+                }
+            }
+        }
+        else{
+            System.out.println("Kullanıcı adı yanlış");
+        }
+    }
+} 
